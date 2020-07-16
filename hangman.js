d89a0ae7439d3fd064cb91289eec7ed19c5649a3
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-07-16 18:13:30 +0530

    Accept answer from users and check

--- hangman.js
+++ hangman.js
@@ -22,12 +22,56 @@ const randomName = () => {
 
 
 function fillInTheBlanks(guessedName) {
-	let name = randomName();
-	console.log("The selected random name is " + name);
-	let status = name.split("").map(char => (guessedName.indexOf(char) >= 0 ? char : " __ ")).join("");
+	console.log("The selected random name is " + selectedName);
+	let status = selectedName.split("")
+		.map(char => (guessedName.indexOf(char) >= 0 ? char : " __ ")).join("");
 		
+	if (status === selectedName) {
+		// say you won.
+	}
+
 	document.getElementById("answer").innerHTML = status;
 }
 
 
-fillInTheBlanks([]);
+function checkEnteredCharacter(inputChar) {
+	userEnteredCharacters.indexOf(inputChar) === -1 ? userEnteredCharacters.push(inputChar) : null;
+
+	if (selectedName.indexOf(inputChar) >= 0) {
+		fillInTheBlanks(userEnteredCharacters);
+	} else {
+		// update the image
+		mistakesCount++;
+		updateHangmanImage();
+	}
+}
+
+
+function updateHangmanImage() {
+	if (mistakesCount >= 6) {
+		document.getElementById("answer").innerHTML = "Name: " + selectedName;
+		document.getElementById("result").innerHTML = "You lost";
+	}
+	document.getElementById("hangman-image").src = "./images/" + mistakesCount + ".jpg";
+}
+
+
+document.onkeypress = function (e) {
+	e = e || window.event;
+	let charCode = e.keyCode || e.which;
+	let charString = String.fromCharCode(charCode);
+	checkEnteredCharacter(charString);
+}
+
+
+let  userEnteredCharacters = [];
+let selectedName = randomName();
+let mistakesCount = 0;
+
+
+function init() {
+	fillInTheBlanks(userEnteredCharacters);
+}
+
+
+init();
